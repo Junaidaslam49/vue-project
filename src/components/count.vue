08f4aeeaf6f0b1b9cb64b3b21cc8b391acda8c57
Junaid Aslam <junaid.aslam@gsquad.io>
2019-09-11 19:44:45 +0500

    added base files for vue

---
+++ src/components/count.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>{{ count }}</div>
+    </div>
+</template>
+<script>
+import {store} from '../store.js'
+export default {
+  name: 'count',
+  computed: {
+    count: function () {
+	    return store.state.count
+    },
+  },
+}
+
+</script>
